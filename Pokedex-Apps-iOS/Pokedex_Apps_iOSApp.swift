ccba598c59060d6745d424d51e6924a92af25f93
reggiegunawan88 <reggiegunawan88@gmail.com>
2021-06-06 15:01:33 +0700

    Init commit

---
+++ Pokedex-Apps-iOS/Pokedex_Apps_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_Apps_iOSApp.swift
+//  Pokedex-Apps-iOS
+//
+//  Created by TI Digital on 06/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_Apps_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
